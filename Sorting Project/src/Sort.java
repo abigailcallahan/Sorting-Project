cb7c709416e41bb842eaa20a8a3edc7d1b269561
abigail.callahan <abigail.callahan@MU-C1-02.mullen.pvt.k12.co.us>
2021-11-30 08:55:44 -0700

    started to generate nums

--- Sorting Project/src/Sort.java
+++ Sorting Project/src/Sort.java
@@ -2,11 +2,40 @@ import java.util.Scanner;
 
 public class Sort
 	{
+		static String enter;
+		static Scanner input = new Scanner(System.in);
+		static int numSim;
+		static int numOfNums;
+		static int nums;
 
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
-
+				askParameters();
+				generateNums();
 			}
+		
+		public static void askParameters()
+		{
+			System.out.println("Welcome to the Sorting Game!");
+			System.out.println("Press enter to start");
+			enter = input.nextLine();
+			
+			System.out.println("How many times should the simulation run?");
+			numSim = input.nextInt();
+			
+			System.out.println("How many numbers should be generated?");
+			numOfNums = input.nextInt();
+			
+		}
 
+		public static void generateNums()
+		{
+			for(int i = 0; i < numOfNums; i++)
+				{
+					nums = (int)(Math.random() * 89) + 10;
+					System.out.println(nums);
+				}
+			
+		}
+		
 	}
