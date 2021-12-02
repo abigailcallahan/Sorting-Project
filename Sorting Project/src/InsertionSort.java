867a8fa42e42d6a2daa82d478f9a078edd3617fb
abigail.callahan <abigail.callahan@MU-C1-02.mullen.pvt.k12.co.us>
2021-12-02 09:03:28 -0700

    working on timing

---
+++ Sorting Project/src/InsertionSort.java
@@ -0,0 +1,24 @@
+import java.util.Date;
+
+public class InsertionSort
+	{
+		public static void insertionSort(int[ ] elements)
+			{
+			 	for (int i = 1; i < elements.length; i++)
+			 		{
+			 		int temp = elements[i];
+			 int possibleIndex = i;
+			 while (possibleIndex > 0 && temp < elements[possibleIndex - 1])
+			 			{
+			 			elements[possibleIndex] = elements[possibleIndex - 1];
+			 			possibleIndex--;
+			 			}
+			 		elements[possibleIndex] = temp;
+			 }
+			 	
+//			 	for(int i = 0; i < elements.length; i++)
+//			 		{
+//			 			System.out.println(elements[i]);
+//			 		}
+			}
+	}
--- Sorting Project/src/Sort.java
+++ Sorting Project/src/Sort.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Date;
 
 public class Sort
 	{
@@ -8,20 +9,41 @@ public class Sort
 		static int numOfNums;
 		static int nums;
 		static int[] arrayNums;
-
+		
+		static int[] sSortArray;
+		static int[] iSortArray;
+		static int[] qSortArray;
+		static int[] mSortArray;
+		static int[] bArray;
+		
+		static Date past = new Date();
+		//sort here
+		static Date future = new Date();
+		
+		//System.out.println("Time (milliseconds) = " + (future.getTime() - past.getTime()));
+		
 		public static void main(String[] args)
 			{
 				askParameters();
-				generateNums();
 				
-//				for(int i = 0; i < numSim; i++)
-//					{
-						//quickSort(arrayNums);
-						selectionSort(arrayNums);
-						//mergeSort();
-						insertionSort(arrayNums);
-						bubbleSort(arrayNums);
-					//}			
+				generateNums();
+
+				sSortArray = arrayNums;
+				iSortArray = arrayNums;
+				qSortArray = arrayNums;
+				mSortArray = arrayNums;
+				bArray = arrayNums;
+				
+				
+				for(int i = 0; i < numSim; i++)
+					{
+						//SelectionSort.selectionSort(sSortArray);
+						MergeSortDemo.mergeSort(mSortArray, mSortArray.length);
+						System.out.println("milliseconds for Merge: " + MergeSortDemo.finalMerge);
+						//QuicksortInt.qsort(qSortArray);
+						//InsertionSort.insertionSort(iSortArray);
+						//BubbleSort.bubbleSort(bArray);
+					}			
 				//printFinal();
 			}
 		
@@ -36,7 +58,6 @@ public class Sort
 			
 			System.out.println("How many numbers should be generated?");
 			numOfNums = input.nextInt();
-			
 		}
 
 		public static void generateNums()
@@ -49,119 +70,7 @@ public class Sort
 				}
 			
 		}
-		
-		//does not work
-		public static int [ ] quickSort(int items[])
-				{
-					qs(items, 0, items.length - 1);
-					return items;
-				}
-			private static void qs(int items[], int left, int right)
-				{
-					int i, j;
-					int pivot, temp;
-					i = left;
-					j = right;
-					pivot = items [(left + right) / 2];
-					do
-						{
-							while ((items [i] < pivot) && (i < right))
-								i++;
-							while ((pivot < items [j]) && (j > left))
-								j--;
-							if (i <= j)
-								{
-									temp = items [i];
-									items [i] = items [j];
-									items [j] = temp;
-									i++;
-									j--;
-									}
-							}
-					while (i <= j);
-					if (left < j)
-						qs (items, left, j);
-					if (i < right)
-						qs (items, i, right);
-					
-					
-					for(int a = 0; a < items.length; a++)
-						{
-							System.out.println(items[a]);
-						}
-					}
-			
-		
-
-		//works
-		public static void selectionSort(int[] elements)
-		{
-			for (int j = 0; j < elements.length - 1; j++)
-				{
-					int minIndex = j;
-					for (int k = j + 1; k < elements.length; k++)
-						{
-							if (elements[k] < elements[minIndex])
-								{
-				 				minIndex = k;
-				 				}
-						}
-					int temp = elements[j];
-				 	elements[j] = elements[minIndex];
-				 	elements[minIndex] = temp;
-				}
-//			for(int i = 0; i < elements.length; i++)
-//				{
-//					System.out.println(elements[i]);
-//				}
-		}
-		
-		//works
-		public static void insertionSort(int[ ] elements)
-			{
-			 	for (int i = 1; i < elements.length; i++)
-			 		{
-			 		int temp = elements[i];
-			 int possibleIndex = i;
-			 while (possibleIndex > 0 && temp < elements[possibleIndex - 1])
-			 			{
-			 			elements[possibleIndex] = elements[possibleIndex - 1];
-			 			possibleIndex--;
-			 			}
-			 		elements[possibleIndex] = temp;
-			 }
-			 	
-//			 	for(int i = 0; i < elements.length; i++)
-//			 		{
-//			 			System.out.println(elements[i]);
-//			 		}
-			}
-
-		//works
-		public static void bubbleSort(int[] arr) 
-			{
-				int n = arr.length;  
-		        int temp = 0;  
-		        for(int i=0; i < n; i++)
-		        	{
-		        		for(int j=1; j < (n-i); j++)
-		        			{
-		        				if(arr[j-1] > arr[j])
-		        					{
-		        						//swap elements
-		        						temp = arr[j-1];
-		        						arr[j-1] = arr[j];
-		        						arr[j] = temp;
-		        					}  
-		                          
-		        			}
-		        	}
-//		        for(int i = 0; i < arr.length; i++)
-//		        	{
-//		        		System.out.println(arr[i]);
-//		        	}
-		    }  
-		
+	
 		public static void printFinal()
 		{
 			System.out.println("Number of Simulations: " + numSim);
